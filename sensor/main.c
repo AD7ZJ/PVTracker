760c7efcd29707cf9f695da9e2c8f4730899a504
Elijah <elijah.brown@cobham.com>
2020-12-28 09:53:48 -0700

    Add serial driver

--- sensor/main.c
+++ sensor/main.c
@@ -1,52 +1,14 @@
-/**
-  Generated Main Source File
+#include <stdint.h>
+#include <xc.h>
+#include <stdio.h>
 
-  Company:
-    Microchip Technology Inc.
-
-  File Name:
-    main.c
-
-  Summary:
-    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
-
-  Description:
-    This header file provides implementations for driver APIs for all modules selected in the GUI.
-    Generation Information :
-        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.6
-        Device            :  PIC18F14K50
-        Driver Version    :  2.00
-*/
-
-/*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
-    may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
-    FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
-    SOFTWARE.
-*/
-
-#include "mcc_generated_files/mcc.h"
+#include "serial.h"
 
 /*
  * Config settings
  */
 
+uint32_t sysTick = 0;
 
 /*
                          Main application
@@ -67,31 +29,71 @@ void main(void)
     ANSEL = 0x00;
     ANSELH = 0x00;
 
-    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
-    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
-    // Use the following macros to:
+    // Enable timer 1
+    T1CONbits.TMR1ON = 0x1;
+    // System clock is not derived from Timer1 Osc
+    T1CONbits.T1RUN = 0x0;
+    // Timer1 Osc is disabled
+    T1CONbits.T1OSCEN = 0x0;
+    // Use Fosc/4 for the clock source
+    T1CONbits.TMR1CS = 0x0;
+    // prescaler of 1:4
+    T1CONbits.T1CKPS = 0x2;
+    // enable its interrupt
+    TMR1IE = 0x01;
 
-    // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
-
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
-
-    // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
-
-    // Disable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptDisable();
-
+    // put the 485 driver in tx
+    PORTC |= 1u << 0;
+    
+    // GIE, PEIE Interrupts
+    INTCON = 0b11000000;
+    //RCIE = 0x01;
+    
+    SerialInit();
+    
+    SerialPutst("Booted\r\n");
+    
     while (1)
     {
-        // Add your application code
-        __delay_ms(500);
-        PORTC |= 1u << 3;
-        __delay_ms(500);
-        PORTC &= ~(1u << 3);
+        if (sysTick >= 10)
+        {
+           PORTC ^= 1u << 3;
+           sysTick = 0;
+        }
     }
 }
+
+void __interrupt() intVector(void) 
+{
+#if 0
+    // Serial receive interrupt
+    if (RCIF) {
+        serbuff = RCREG;
+        if (serMode != STARTUP) {
+            FifoWrite(serbuff);
+        }
+
+        // clear any overrun errors
+        if (OERR)
+        {
+            CREN=0;
+            CREN=1;
+        }
+    }
+#endif
+
+    // Timer 1 interrupt every 50ms
+    if (TMR1IF) 
+    {
+        // Clear interrupt flag & reload timer
+        TMR1IF = 0x00;
+        // we need to divide 1 Mhz by 50,000 to get a 20ms tick. So, reload 65535-50000=0x3CAF
+        TMR1H = 0x3C;
+        TMR1L = 0xAF;
+        sysTick++;
+    }
+}
+
 /**
  End of File
 */
---
+++ sensor/serial.c
@@ -0,0 +1,172 @@
+#include <xc.h>
+#include "serial.h"
+
+/**
+ *
+ * @defgroup library Generic Library Functions
+ *
+ * @{
+ */
+
+
+/// CPU clock speed in Hz
+#define PIC_CLK 16000000
+
+/// desired serial baud rate
+#define BAUD 9600
+
+/// calculate the baud rate generator divider
+#define DIVIDER ((PIC_CLK/(16UL * BAUD) -1))
+
+/// defines whether to use high speed baud rates or not (setting 0 changes the divider calc)
+#define HIGH_SPEED 1
+
+static unsigned char dummy;
+
+/**
+ * Initializes the onboard serial port hardware
+ */
+void SerialInit(void) {
+    SPBRG = DIVIDER;    //using the baudrate generator in 8-bit mode
+    BRGH  = HIGH_SPEED; //data rate for sending
+    SYNC  = 0; //asynchronous
+    SPEN  = 1; //enable serial port pins
+    CREN  = 1; //enable reception
+    TXIE  = 0; //disable tx interrupts
+    RCIE  = 0; //disable rx interrupts
+    TX9   = 0;  //8-bit transmission
+    RX9   = 0;  //8-bit reception
+    TXEN  = 0; //reset transmitter
+    TXEN  = 1; //enable the transmitter
+}
+
+/// macro for clearing any UART errors
+#define clear_usart_errors_inline    \
+if (OERR)                \
+{                        \
+    TXEN=0;              \
+    TXEN=1;              \
+    CREN=0;              \
+    CREN=1;              \
+}                        \
+if (FERR)                \
+{                        \
+    dummy=RCREG;         \
+    TXEN=0;              \
+    TXEN=1;              \
+}
+
+/**
+ * Write a character to the serial port (necessary to use embedded printf calls).
+ *
+ * @param c character to write
+ */
+void putch(unsigned char c) {
+    while (!TXIF) //set when register is empty
+    {
+        clear_usart_errors_inline;
+        CLRWDT();
+    }
+    TXREG = c;
+}
+
+/**
+ * Get a character from the serial port without timeout (neccessary to use
+ * embedded scanf calls).
+ *
+ * @return character received from the UART
+ */
+unsigned char getch(void) {
+    while (!RCIF) {
+        CLRWDT();
+        clear_usart_errors_inline;
+    }
+    return RCREG;
+}
+
+/**
+ * Public function for clearing any framing or overrun errors from the serial
+ * port hardware.
+ */
+void SerialClearErrors(void) {
+    clear_usart_errors_inline;
+}
+
+/**
+ * Write a character to the serial port in hex.
+ *
+ * @param c 8 bit value to output in hex
+ */
+void SerialPutCharHex(unsigned char c) {
+    unsigned char temp;
+
+    // transmits in hex
+    temp = c;
+
+    c = (c >> 4);
+    if (c < 10) c += 48;
+    else c += 55;
+    putch(c);
+
+    c = temp;
+
+    c = (c & 0x0F);
+    if (c < 10) c += 48;
+    else c += 55;
+    putch(c);
+}
+
+/**
+ * Write an integer (16 bit) out to the serial port in hex.
+ *
+ * @param c Integer value to be written
+ */
+void SerialPutIntHex(unsigned int c) {
+#define ramuint(x)              (*((unsigned int *) (x)))
+#define ramuint_hibyte(x)       (*(((unsigned char *)&x)+1))
+#define ramuint_lobyte(x)       (*(((unsigned char *)&x)+0))
+#define ramuchar(x)             (*((unsigned char *) (x)))
+
+    SerialPutCharHex(ramuint_hibyte(c));
+    SerialPutCharHex(ramuint_lobyte(c));
+
+#undef ramuint
+#undef ramuint_hibyte
+#undef ramuint_lobyte
+#undef ramuchar
+}
+
+
+/**
+ * Write an 8 bit value to the serial port in decimal form
+ *
+ * @param c 8 bit value to be output.
+ */
+void SerialPutCharDec(unsigned char c) {
+    unsigned char temp;
+
+    temp = c;
+    //hundreds
+    if ((c / 100) > 0) putch((c / 100) + '0');
+    c -= (c / 100)*100;
+
+    //tens
+    if (((temp / 10) > 0) || ((temp / 100) > 0)) putch((c / 10) + '0');
+    c -= (c / 10)*10;
+
+    //ones
+    putch((c / 1) + '0');
+}
+
+/**
+ * Write a string to the serial port.
+ * @param str String to be written.  
+ */
+void SerialPutst(register const char *str) {
+    while ((*str) != 0) {
+        putch(*str);
+        str++;
+    }
+}
+
+/** @} */
